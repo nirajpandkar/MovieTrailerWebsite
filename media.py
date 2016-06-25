3b8d097e2a36ccd3403d705c9767a6dd3f34f51e
nirajpandkar <niraj.pandkar@gmail.com>
2016-06-26 00:16:17 +0530

    added some more attributes for the movies

--- media.py
+++ media.py
@@ -1,26 +1,43 @@
 import webbrowser
 import requests
-import urllib
-
+from bs4 import BeautifulSoup
 
 class Movie:
-    def __init__(self, movie_name, movie_trailer):
-        self.trailer_youtube_url = movie_trailer
+    def __init__(self, movie_name):
+        self.trailer_youtube_url = ""
         self.title = ""
         self.poster_image_url = ""
         self.storyline = ""
         self.imdb_rating = ""
+        self.directors = ""
+        self.stars = ""
+        self.writers = ""
         self.movie_info(movie_name)
+        self.trailer_youtube_url = self.trailer_url(movie_name)
 
     def movie_info(self, title):
-        parameter = {'t': title}
-        req = requests.get(
-            'http://www.omdbapi.com/?plot=short&type=movie&r=json&' +
-            str(urllib.urlencode(parameter)))
-        self.title = str(req.json()['Title'])
-        self.poster_image_url = str(req.json()['Poster'])
-        self.storyline = str(req.json()['Plot'])
-        self.imdb_rating = str(req.json()['imdbRating'])
+        resp = requests.get('http://www.omdbapi.com/', params={
+            't': title,
+            'type': 'movie',
+            'plot': 'short'
+        })
+        self.title = str(resp.json()['Title'])
+        self.poster_image_url = str(resp.json()['Poster'])
+        self.storyline = str(resp.json()['Plot'])
+        self.imdb_rating = str(resp.json()['imdbRating'])
+        self.directors = str(resp.json()['Director'])
+        self.writers = str(resp.json()['Writer'])
+        self.stars = str(resp.json()['Actors'])
+
+    def trailer_url(self, title):
+        response = requests.get("https://youtube.com/results", params={
+            'search_query': title + " trailer"
+        })
+
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        for video in soup.findAll(attrs={'class': 'yt-uix-tile-link'}):
+            return "https://youtube.com" + video["href"]
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
--- media_center.py
+++ media_center.py
@@ -1,17 +1,17 @@
 import media
-import fresh_tomatoes
+#import fresh_tomatoes
+import test
 
-penguins_of_madagascar = media.Movie("Penguins Of Madagascar",
-                                     "https://youtu.be/retX8Wj7JdM")
+penguins_of_madagascar = media.Movie('Penguins of madagascar')
+epic = media.Movie('Epic')
+big_hero_6 = media.Movie('Big Hero 6')
+kingsman = media.Movie('Kingsman')
+home = media.Movie('Home')
+the_lego_movie = media.Movie('The lego movie')
+the_social_network = media.Movie('The Social Network')
+the_theory_of_everything = media.Movie('The Theory of Everything')
 
-epic = media.Movie("Epic", "https://youtu.be/BJVkoq_wK80")
+movies = [penguins_of_madagascar, epic, big_hero_6, home, kingsman,
+          the_lego_movie, the_social_network, the_theory_of_everything]
 
-big_hero_6 = media.Movie("Big Hero 6", "https://youtu.be/z3biFxZIJOQ")
-
-home = media.Movie('Home', "https://youtu.be/MyqZf8LiWvM")
-
-kingsman = media.Movie('Kingsman', "https://youtu.be/kl8F-8tR8to")
-
-movies = [penguins_of_madagascar, epic, big_hero_6, home, kingsman]
-
-fresh_tomatoes.open_movies_page(movies)
+test.open_movies_page(movies)
